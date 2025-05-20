973171257ca9f1a5f00d7ce6a5858ef636b194c3
kaua hedlund <hedlundkaua@gmail.com>
2025-05-20 11:25:05 -0300

    Atualização da atividade

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,58 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.entities.Product;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Locale.setDefault(Locale.US);
+        Scanner sc= new Scanner(System.in);
+        String lerArquivo = "C:\\Windows\\Temp\\input.csv";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        List<Product> list = new ArrayList<>();
+
+        System.out.print("Entre com o caminho do arquivo: ");
+        String sourceFileStr = sc.nextLine();
+
+        //encapsula o processo de acessar os arquivos e trabalhar com os mesmos
+        File sourceFile = new File(sourceFileStr);
+        //pega o nome do caminho do arquivo, sem o arquivo
+        String sourceFolderStr = sourceFile.getParent();
+
+        //cria a pasta
+        boolean success = new File(sourceFolderStr + "\\out").mkdir();
+        //cria o arquivo na pasta criada
+        String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
+
+        //tenta ler o sourceFileStr
+        try (BufferedReader bf = new BufferedReader(new FileReader(sourceFileStr))){
+            String itemCsv = bf.readLine();
+            while (itemCsv != null){
+                String[] fields = itemCsv.split(",");
+                String name = fields[0];
+                double price = Double.parseDouble(fields[1]);
+                int quantity = Integer.parseInt(fields[2]);
+
+                list.add(new Product(name, price, quantity));
+
+                itemCsv = bf.readLine();
+            }
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))){
+                for(Product item : list){
+                    bw.write(item.getName() + "," + String.format("%.2f",item.total()) + "," + item.getQuantity());
+                    bw.newLine();
+                }
+                System.out.println(targetFileStr +" CEATED");
+            }catch (IOException e){
+                System.out.println("Error writing file: " + e.getMessage());
+            }
+
+        }catch (IOException e){
+            System.out.println("Error writing file: " + e.getMessage());
         }
     }
 }
